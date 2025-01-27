0fec445f0042aecd0652da9714f5b89a171fbd37
matteodragotto <matteodrag8@gmail.com>
2025-01-27 17:07:59 +0100

    [feat] cards now receive data from post prop

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,15 +1,17 @@
-import { usePostsContext } from "../context/PostContext"
 
-const PostCard = () => {
+
+const PostCard = ({ post }) => {
 
   return (
     <div className="card">
-      <img className="img-fluid" src="" alt="" />
+      <img className="img-fluid" src={post.image} alt={post.title} />
       <div className="card-body">
-        <h5 className="card-text"></h5>
-        <p className="card-text"></p>
-        <h6>Tag:</h6>
-        <p className="card-text"></p>
+        <h5 className="card-text">
+          {post.title}
+        </h5>
+        <p className="card-text">
+          {post.content}
+        </p>
       </div>
     </div>
   )
